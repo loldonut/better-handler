5b1131325fa845000955d47439ab4c1f4842d565
loldonut <loldonutph@gmail.com>
2024-04-03 16:04:23 +0800

    use traditional for loops instead

--- src/CommandHandler.ts
+++ src/CommandHandler.ts
@@ -151,12 +151,12 @@ export default class CommandHandler extends EventEmitter {
 
         this.Log('Registering Commands...');
 
-        commandFiles.forEach((file) => {
+        for (const file of commandFiles) {
             const command = require(`${path}/${file}`) as CommandOptions; // eslint-disable-line
 
             this.commands.set(command.name, command);
             this.Log(`Loaded command ${command.name}`);
-        });
+        }
 
         this.Log('Registered Commands.');
     }
